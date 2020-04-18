83747da20f5fbff439e1870d88ba62c71f92911d
Borisko130 <krimenyukb@gmail.com>
2020-04-18 12:17:04 +0300

    changed test file

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {});
+it('renders without crashing', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
