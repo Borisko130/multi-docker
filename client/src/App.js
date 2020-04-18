208c7d6881076b2a48492faecccd1848776c6341
Borisko130 <krimenyukb@gmail.com>
2020-04-18 13:27:52 +0300

    added dependencies in client/package.json

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import 'react-router-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
